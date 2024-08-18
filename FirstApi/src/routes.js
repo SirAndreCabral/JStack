77be30358c7e75b18567853c6450aec475c5f229
SirAndreCabral <andre_kbral@hotmail.com>
2024-08-18 04:39:27 -0300

    first api

---
+++ FirstApi/src/routes.js
@@ -0,0 +1,17 @@
+const UserController = require("./controllers/UserController");
+
+module.exports = [
+    {
+        endpoint: '/users',
+        method: 'GET',
+        handler: UserController.listUsers,
+    },
+    {
+        endpoint: '/users',
+        method: 'POST',
+    },
+    {
+        endpoint: '/users',
+        method: 'P',
+    },
+];
