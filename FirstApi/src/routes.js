25bb6580a7956e0fc0eca7db39415f606df3a824
SirAndreCabral <andre_kbral@hotmail.com>
2024-08-24 23:44:53 -0300

    first api

--- FirstApi/src/routes.js
+++ FirstApi/src/routes.js
@@ -14,9 +14,16 @@ module.exports = [
     {
         endpoint: '/users',
         method: 'POST',
+        handler: UserController.createUser,
     },
     {
-        endpoint: '/users',
+        endpoint: '/users/:id',
         method: 'PUT',
+        handler: UserController.updateUser,
+    },
+    {
+        endpoint: '/users/:id',
+        method: 'DELETE',
+        handler: UserController.deleteUser,
     },
 ];
